a7d9c435b6e03d92969daf44b04395019743a344
chamran <chamran.merega@gmail.com>
2022-08-09 19:55:53 +0300

    Delete last zero element in table

--- index.py
+++ index.py
@@ -11,10 +11,13 @@ allow_file = settings.allow_file
 def showip():
     file = open(allow_file, 'r')
     data = file.read()
+    data = data.split(";")
+    data.pop()
+    # data = l.pop(data)
     file.close()
-    hstnm = subprocess.check_output("/usr/bin/hostname")
+    hstnm = subprocess.check_output("hostname")
     #return(data.split(";"))
-    return render_template('showip.html', data=data.split(";"), hstnm = hstnm.decode("utf-8"), allow_file = allow_file)
+    return render_template('showip.html', data=data, hstnm = hstnm.decode("utf-8"), allow_file = allow_file)
 
 
 @app.route('/addip')
@@ -32,12 +35,12 @@ def addip():
             new_data = old_data.replace('deny all;', 'allow {ipadr};\ndeny all;'.format(ipadr = ipadr))
             with open(allow_file, 'w') as f:
                 f.write(new_data)
-        ngnx = os.system('/usr/sbin/nginx -t')
+        ngnx = os.system('nginx -t')
         if ngnx != 0:
             with open(allow_file, 'w') as f:
                 f.write(old_data)
         else:
-            os.system('/usr/bin/systemctl reload nginx')
+            os.system('systemctl reload nginx')
         
         return render_template('added.html', ipadr=ipadr, ngnx=ngnx)
 
